185d0671f402f826c8c5de9663ee3556302311e9
Urfeena <urfeena@preksh.com>
2017-11-08 17:54:15 +0530

    first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  //Templating
+  <div>
+    //Property Binding
+    <h1>Employees Record</h1>
+    <ul *ngFor = " let emp of employees">
+      <h2>{{emp.sno}}</h2>
+      <li>{{emp.name}}</li>
+      <li>{{emp.gender}}</li>
+      <li>{{emp.location}}</li>
+    </ul>
+    //Event Binding
+    <button (click) = myClick($event)> Click Me</button>
+  </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  employees = [
+    {sno: '1', name: 'John', gender: 'Male', location: 'USA'},
+    {sno: '2', name: 'Lara' , gender: "Female", location: "NY"},
+    {sno: '3', name: 'Lisa', gender: "Female", location: "NZ"}
+  ];
+
+  myClick(event){
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    GalleryComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule
